5e5e82fdbd3111bce7df076a70c23281a7500a63
George Gaylor <GeorgeGaylor83@gmail.com>
2017-09-05 09:01:34 -0700

    Filled in server.js so we can fire up Heroku

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const express = require('express');
+const requestProxy = require('express-request-proxy');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('./public'));
+
+app.listen(PORT, function() {
+  console.log(`My app is listening on port ${PORT} !`);
+});
