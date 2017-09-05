efbd2352072517690891dff67e1ce9148b5abb93
kernamx <powen.chang1@gmail.com>
2017-09-05 14:27:38 -0700

    working on getting token

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
 
+app.get('/data/*', proxySeattle);
+function proxySeattle(request, response){
+  (requestProxy({
+    url: 'https://data.seattle.gov/resource/47rs-c243.json',
+    headers: {
+      '$limit' : 5000,
+      '$$app_token' : '${process.env.SEATTLE_TOKEN}'}
+  }))(request, response);
+}
+
 app.listen(PORT, function() {
   console.log(`My app is listening on port ${PORT} !`);
 });
