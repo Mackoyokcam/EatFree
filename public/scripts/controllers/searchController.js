9b0623fb842206791ef46cbc25bd43b791371095
vgalenko <vitaliygalenko@gmail.com>
2017-09-05 13:49:31 -0700

    added IFFY function to both pages, and added constructor function

--- public/scripts/controllers/searchController.js
+++ public/scripts/controllers/searchController.js
@@ -1,1 +1,12 @@
-//ajax request goes here 
+
+//ajax request goes here
+
+var app = app || {};
+
+$.ajax({
+	url: '', //google api
+	method:'GET',
+	headers: {
+		Authorization: '' //token goes here
+	}
+}).then(data =>);
--- public/scripts/model/mealData.js
+++ public/scripts/model/mealData.js
@@ -1,1 +1,23 @@
-//constructor goes here
+'use strict';
+
+var app = app || {};
+
+(function(module) {
+function Mealdata(dayTime, location, mealServed, nameOfProgram, peopleServed) {
+  this.dayTime = dayTime;
+  this.location = location;
+  this.mealServed = mealServed;
+  this.nameOfProgram = nameOfProgram;
+  this.peopleServed = peopleServed;
+}
+
+Mealdata.all = [];
+
+Mealdata.getData = function(callback) {
+  $.get('/data', callback); //talk to george about that
+};
+
+
+module.Mealdata = Mealdata;
+
+})(app);
