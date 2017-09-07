e73405b57c2933d95d6b2d930247d10452d25bb8
mackoyokcam <mark.hangarter@gmail.com>
2017-09-07 16:16:51 -0700

    we have a map! it can center on entered address

--- public/scripts/models/maps.js
+++ public/scripts/models/maps.js
@@ -1,23 +1,56 @@
 'use strict';
+var app = app || {};
 
-// const mapOptions = {
-//   center: new google.maps.LatLng(37.7831, -122.4039),
-//   zoom: 12,
-//   mapTypeId: google.maps.MapTypeId.ROADMAP
-// }
-// const map = new google.maps.Map(document.getElementById('map'), mapOptions);
-//
-//
-// const geocoder = new google.maps.Geocoder();
-//
-// function centerOnLocation(address) {
-//       //In this case it creates a marker, but you can get the lat and lng from the location.LatLng
-//       map.setCenter(address);
-//       var marker = new google.maps.Marker({
-//         map: map,
-//         position: address
-//       });
-// }
+var map;
+var pinImage;
+var pinShadow;
+var pinColor;
+var latlng;
+
+function myMap() {
+
+  pinColor = 'FE7569';
+  pinImage = new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|' + pinColor,
+      new google.maps.Size(21, 34),
+      new google.maps.Point(0,0),
+      new google.maps.Point(10, 34));
+  pinShadow = new google.maps.MarkerImage('http://chart.apis.google.com/chart?chst=d_map_pin_shadow',
+      new google.maps.Size(40, 37),
+      new google.maps.Point(0, 0),
+      new google.maps.Point(12, 35));
+
+  let mapOptions = {
+    center: new google.maps.LatLng(37.7831, -122.4039),
+    zoom: 8,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+}
+
+
+function centerOnLocation(address) {
+  let geocoder = new google.maps.Geocoder();
+  geocoder.geocode({'address': address}, function(results, status) {
+    if (status === google.maps.GeocoderStatus.OK) {
+      let lat = results[0].geometry.location.lat();
+      let long = results[0].geometry.location.lng();
+      var latlng = new google.maps.LatLng(lat, long);
+      console.log(latlng);
+      map.setCenter(latlng);
+      let marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        icon: pinImage,
+        shadow: pinShadow
+      });
+      map.setZoom(17);
+      map.panTo(marker.position);
+    } else {
+      console.error('Geocode failed.');
+    }
+  });
+}
 
 
 // const mapOptions = {
